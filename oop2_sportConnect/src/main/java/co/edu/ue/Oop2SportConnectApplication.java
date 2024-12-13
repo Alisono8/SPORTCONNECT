37666bc39b52523927e96e50458f2dab43259018
nicolasmen99 <nmenjura@uniempresarial.edu.co>
2024-12-12 21:32:41 -0500

    Oop2SportConnectApplication
    @SpringBootApplication
    @EntityScan(basePackages={"co.edu.ue.model"})
    @EnableJpaRepositories(basePackages={"co.edu.ue.repository"})
    @ComponentScan(basePackages={"co.edu.ue.service","co.edu.ue.controller","co.edu.ue","co.edu.ue.security"})
    public class Oop2SportConnectApplication {

--- oop2_sportConnect/src/main/java/co/edu/ue/Oop2SportConnectApplication.java
+++ oop2_sportConnect/src/main/java/co/edu/ue/Oop2SportConnectApplication.java
@@ -2,8 +2,15 @@ package co.edu.ue;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 
 @SpringBootApplication
+@EntityScan(basePackages={"co.edu.ue.model"})
+@EnableJpaRepositories(basePackages={"co.edu.ue.repository"})
+@ComponentScan(basePackages={"co.edu.ue.service","co.edu.ue.controller","co.edu.ue","co.edu.ue.security"})
 public class Oop2SportConnectApplication {
 
 	public static void main(String[] args) {
