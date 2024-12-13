5838870d380149a1627b75b02fc33a1d442c12ae
nicolasmen99 <nmenjura@uniempresarial.edu.co>
2024-12-13 01:19:21 -0500

    otro

--- oop2_sportConnect/src/main/java/co/edu/ue/service/EventoService.java
+++ oop2_sportConnect/src/main/java/co/edu/ue/service/EventoService.java
@@ -2,34 +2,38 @@ package co.edu.ue.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import co.edu.ue.model.Eventos;
+import co.edu.ue.repository.IEvento;
 @Service
 public class EventoService implements IEventoService {
 
+	@Autowired
+	IEvento dao;
 	@Override
 	public List<Eventos> agregarEvento(Eventos evento) {
 		// TODO Auto-generated method stub
-		return null;
+		return dao.addEvento(evento);
 	}
 
 	@Override
 	public Eventos actualizarEvento(Eventos evento) {
 		// TODO Auto-generated method stub
-		return null;
+		return dao.updateEvento(evento);
 	}
 
 	@Override
 	public List<Eventos> buscarEventos() {
 		// TODO Auto-generated method stub
-		return null;
+		return dao.getAllEventos();
 	}
 
 	@Override
 	public boolean bajaEvento(int id) {
 		// TODO Auto-generated method stub
-		return false;
+		return dao.deleteEvento(id);
 	}
 
 	@Override
