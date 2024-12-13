7d54f8cf1ff536f6c38681ed755c295132ed1b96
nicolasmen99 <nmenjura@uniempresarial.edu.co>
2024-12-13 00:26:28 -0500

    otro

--- oop2_sportConnect/src/main/java/co/edu/ue/service/IDetalleEventoService.java
+++ oop2_sportConnect/src/main/java/co/edu/ue/service/IDetalleEventoService.java
@@ -1,5 +1,19 @@
 package co.edu.ue.service;
 
+import java.util.List;
+
+import co.edu.ue.model.DetalleEvento;
+
 public interface IDetalleEventoService {
 
+
+	List<DetalleEvento> DetalleEvento(DetalleEvento detalleEvento);
+	
+	DetalleEvento actualizarDetalleEvento(DetalleEvento detalleEvento);
+	
+	List<DetalleEvento> buscarEvento();
+	
+	boolean bajaDetalleEvento(int id);
+	
+	DetalleEvento buscarIdDDetalleEvento(int id);
 }
